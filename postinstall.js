f58ed61e200f229e3b2b982ff8e92346d6699c5a
Jam <yedsn@163.com>
2022-10-02 00:16:18 +0800

    更新git地址

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,6 @@
 var env = process.env
 
-var BANNER = '\x1b[33mThank you for using vue-easy-dict (\x1b[32m https://gitee.com/hongxiaojian/vue-easy-dict \x1b[96m)\n' +
+var BANNER = '\x1b[33mThank you for using vue-easy-dict (\x1b[32m https://github.com/yedsn/vue-easy-dict \x1b[96m)\n' +
   '\x1b[35mAuthor: Jam<yedsn@163.com>\x1b[0m\n'
 
 // eslint-disable-next-line no-console,no-control-regex
